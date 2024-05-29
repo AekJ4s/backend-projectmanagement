fabd921c9706d3e1cc999cfd1a77e76b3938fe60
AekJ4s <Jasdakornam@gmail.com>
2024-05-29 11:52:53 +0700

    FinshWebsiteBackend

--- obj/Debug/net8.0/backend-ProjectManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend-ProjectManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b25ae204ff81f3b217135f9f15f91238406c91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0a51cdb7d096c81a5c65c6ef5e90446ecd58c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
